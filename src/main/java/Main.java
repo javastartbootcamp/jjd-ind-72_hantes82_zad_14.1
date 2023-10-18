9308d96594218c7a3e814a5bd4dc3b7b1a4f8b4f
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-10-18 09:47:11 +0200

    Zadanie zrobione

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String filePath = "src/main/Resources/liczby.txt";
+        String filePath = "/Users/konrad/Documents/Java/jjd-ind-72_hantes82_zad_14.1/liczby.txt";
         Statistics.printStats(filePath);
 
     }
